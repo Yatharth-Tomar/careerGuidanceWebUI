006c71d5c4c96322b92b86e86626f99de4d9ed79
Yatharth Tomar <yatharth_tomar@srmap.edu.in>
2024-03-10 02:31:48 +0530

    Added 404 not foung page with proper navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Footer from "./Components/Footer";
 
-import { Routes,Route } from 'react-router-dom'
-import './App.css'
-import Footer from './Components/Footer'
-
-import HomePage from './Pages/HomePage'
-import AboutUS from './Pages/AboutUs'
-
+import HomePage from "./Pages/HomePage";
+import AboutUS from "./Pages/AboutUs";
+import NotFound from "./Pages/NotFound";
 
 function App() {
-  
-
   return (
     <>
-    <Routes>
-      <Route path="/" element={<HomePage/>}></Route>
-      <Route path="/aboutUs" element={<AboutUS/>}></Route>
-    
-      
-    </Routes>
-   
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/aboutUs" element={<AboutUS />}></Route>
 
-
-    
-
-    
-      
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Pages/NotFound.jsx
@@ -0,0 +1,40 @@
+import { Link, useNavigate } from "react-router-dom";
+
+function NotFound() {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <div className="text-center p-10">
+        <h1 className="mb-4 text-6xl font-semibold text-red-500">404</h1>
+        <p className="mb-4 text-lg text-gray-600">
+          Oops! Looks like you're lost.
+        </p>
+        <div className="animate-bounce">
+          <svg
+            className="mx-auto h-16 w-16 text-red-500"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
+            ></path>
+          </svg>
+        </div>
+        <p className="mt-4 text-gray-600">
+          Let's get you back{" "}
+          <span onClick={() => navigate(-1)} className="text-blue-500">
+            home
+          </span>
+          .
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default NotFound;
